8937b59ac71048f7732c42cb172baf7aef31ce5b
saidada <onco-seijin@hotmail.co.jp>
2017-12-03 21:50:53 +0900

    デザインを色々考えてたら失敗作が出来た。今度作り直し。

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="stylesheet.css" type="text/css">
+  <title>Saidada Bbs</title>
+</head>
+<body>
+  <img class="title" src="img/titleLogo.png">
+  <form>
+
+    <div class="box">
+      <div class="form">
+        <input type="text" class="name" placeholder="お名前">
+        <img class="formBorder" src="img/messageFormBorder.png">
+        <textarea class="message" name="message" placeholder="メッセージ"></textarea>
+      </div>
+
+      <div class="button">
+        <input type="submit" class="send" value="書き込む">
+        <br>
+
+        <div class="radioBorder">
+          <label>
+            <input type="radio" name="q1" class="radio">
+            <img class="radio_image" src="img/characterCat1.png">
+          </label>
+          <label>
+            <input type="radio" name="q1" class="radio">
+            <img class="radio_image" src="img/characterCat2.png">
+          </label>
+          <label>
+            <input type="radio" name="q1" class="radio">
+            <img class="radio_image" src="img/characterCat3.png">
+          </label>
+        </div>
+
+      </div>
+
+    </div>
+
+
+  </form>
+
+  <hr>
+
+  <ul>
+    <div class="display">
+      <li>レイアウトを考えて貰いましたが、</li>
+      <li>画像をそのまま使っているので、</li>
+      <li>配置などが上手く行かなく、無理な気がしてきました。</li>
+      <li>デザインをやり直します。</li>
+      <li class="displayName">西打 新聖</li>
+    </div>
+    <li>
+      <img  class="displayImg" src="img/characterCat3.png">
+    </li>
+  </ul>
+  <br>
+
+  <ul>
+    <div class="display">
+      <li>はじめまして、炭酸飲料界隈から来ました。</li>
+      <li>この度は、この掲示板に書き込みさせて頂けるとの事で、</li>
+      <li>大変光栄に考えております。</li>
+      <li>じゃあな</li>
+      <li class="displayName">西打 新聖</li>
+    </div>
+    <li>
+      <img  class="displayImg" src="img/characterCat2.png">
+    </li>
+  </ul>
+
+</body>
+</html>
