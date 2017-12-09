9f172be80afe72402b649d964677b200f27d1891
saidada <onco-seijin@hotmail.co.jp>
2017-12-09 18:51:57 +0900

    作り直し。何故か某巨大掲示板風になってしまった気がします。

--- index.php
+++ index.php
@@ -1,75 +1,129 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+// CSFR
+session_start();
+
+function setToken() {
+  $token = sha1(uniqid(mt_rand(), true));
+  $_SESSION['token'] = $token;
+}
+
+function checkToken() {
+  if (empty($_SESSION['token']) || ($_SESSION['token'] != $_POST['token'])) {
+    echo "不正なPOST";
+    exit;
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name']) && isset($_POST['message'])) { // isset :変数がセットされて、かつNULLではない場合に TRUE
+
+  checkToken();
+
+  $name = $_POST['name'];
+  $message = $_POST['message'];
+
+  if($_POST['name'] == ""){ // 名前を空欄を回避
+    $name = "名無しサイダー";
+  }
+
+  $logFile = "log.tsv";
+  $fileData = fopen($logFile, "ab");
+
+  $count = count(file($logFile)) + 1;
+
+  if ($fileData) { // ファイルを開くのに成功していたら
+    flock($fileData, LOCK_EX); // 書き込みロック
+    if ($message) {
+
+      $name = str_replace("\t", ' ', $name); //tab回避
+      $message = str_replace("\t", ' ', $message);
+      $message = str_replace("\n", '<br>', $message);
+
+      $writeDate = date('Y/n/j H:i:s');
+      fwrite($fileData, "No." . $count . "\t" . $name . "\t" . $writeDate . "\t" . $message . "\n");
+    }
+    fclose($fileData);
+  }
+} else {
+  setToken();
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="stylesheet.css" type="text/css">
-  <title>Saidada Bbs</title>
+  <title>サイダダ掲示板</title>
 </head>
 <body>
-  <img class="title" src="img/titleLogo.png">
-  <form>
-
-    <div class="box">
-      <div class="form">
-        <input type="text" class="name" placeholder="お名前">
-        <img class="formBorder" src="img/messageFormBorder.png">
-        <textarea class="message" name="message" placeholder="メッセージ"></textarea>
-      </div>
-
-      <div class="button">
-        <input type="submit" class="send" value="書き込む">
-        <br>
-
-        <div class="radioBorder">
-          <label>
-            <input type="radio" name="q1" class="radio">
-            <img class="radio_image" src="img/characterCat1.png">
-          </label>
-          <label>
-            <input type="radio" name="q1" class="radio">
-            <img class="radio_image" src="img/characterCat2.png">
-          </label>
-          <label>
-            <input type="radio" name="q1" class="radio">
-            <img class="radio_image" src="img/characterCat3.png">
-          </label>
-        </div>
-
-      </div>
-
-    </div>
-
-
-  </form>
+  <h1>サイダダ掲示板</h1>
+  <h2>こんにちは、仲良く書き込んで下さい。</h2>
+  <h2>間違って書き込みしても削除できませんので、あしからず。（頑張れば出来ます）</h2>
 
   <hr>
 
-  <ul>
-    <div class="display">
-      <li>レイアウトを考えて貰いましたが、</li>
-      <li>画像をそのまま使っているので、</li>
-      <li>配置などが上手く行かなく、無理な気がしてきました。</li>
-      <li>デザインをやり直します。</li>
-      <li class="displayName">西打 新聖</li>
-    </div>
-    <li>
-      <img  class="displayImg" src="img/characterCat3.png">
-    </li>
-  </ul>
-  <br>
+  <form action="index.php" method="post">
+    <table>
+      <tr>
+        <td>
+          <input type="text" name="name" placeholder="お名前">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <textarea name="message" placeholder="メッセージ"></textarea>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" value="書き込む">
+          <input type="hidden" name="token" value=<?php echo ($_SESSION['token']); ?>>
+        </td>
+      </tr>
+    <table>
+  </form>
 
-  <ul>
-    <div class="display">
-      <li>はじめまして、炭酸飲料界隈から来ました。</li>
-      <li>この度は、この掲示板に書き込みさせて頂けるとの事で、</li>
-      <li>大変光栄に考えております。</li>
-      <li>じゃあな</li>
-      <li class="displayName">西打 新聖</li>
-    </div>
-    <li>
-      <img  class="displayImg" src="img/characterCat2.png">
-    </li>
-  </ul>
+
+<hr>
+
+<?php
+  $logFile = "log.tsv";
+
+  $display = file($logFile, FILE_IGNORE_NEW_LINES);
+  $display = array_reverse($display);
+
+  foreach ($display as $post) {
+    $cols = explode("\t", $post);
+
+    echo "<div>";
+    echo "<span class=\"writeNumber\">";
+    echo $cols[0] . " ";
+    echo "</span>";
+    echo "<span class=\"name\">";
+    echo $cols[1] . " ";
+    echo "</span>";
+    echo "<span class=\"writeDate\">";
+    echo $cols[2];
+    echo "</span>";
+    echo "</div>";
+
+    echo "<div>";
+    echo "<span class=\"message\">";
+    echo $cols[3];
+    echo "</span>";
+    echo "<div>";
+    echo "<hr>";
+
+  }
+?>
+
 
 </body>
 </html>
